0e39afff3f565210653513a23fb83fb8b5cd3c69
Ray <a@kct.me>
2019-03-17 03:30:25 +0300

    All 2x2 GOVNOCODE ready

--- hello.py
+++ hello.py
@@ -4,58 +4,21 @@
 
 #input block matrix
 
-A = [2, 1, 4, 3]
+inp = input('Add Matix A, separated by space:')
+A = list(map(float, inp.split()))
+
+#A = [0, 0, 0, 0]
 
 print ('Matrix A is:', A)
 
 #input block matrix B
 
-B = [-13, 26, -63, 44]
+inp = input('Add Matix B, separated by space:')
+B = list(map(float, inp.split()))
+
+#B = [1, 2, 3, 4]
 
 print ('Matrix B is:', B)
-
-#matrix determ func 2
-
-def matrix_determ(mataorb):
-    return mataorb[0] * mataorb[3] - mataorb[2] * mataorb[1]
-
-det_A = matrix_determ(A)
-det_B = matrix_determ(B)
-
-print ("Determent of matrix A is:",det_A)
-print ("Determent of matrix B is:",det_B)
-
-#matrix -1 inverse 2
-
-def matrix_inverse(mataorb, detaorb):
-    return 1/detaorb * mataorb[3]
-
-A0 = matrix_inverse(A, det_A)
-B0 = matrix_inverse(B, det_B)
-
-def matrix_inverse_1(mataorb, detaorb):
-    return 1/detaorb * -1 * mataorb[1]
-
-A1 = matrix_inverse_1(A, det_A)
-B1 = matrix_inverse_1(B, det_B)
-
-def matrix_inverse_2(mataorb, detaorb):
-    return 1/detaorb * -1 * mataorb[2]
-
-A2 = matrix_inverse_2(A, det_A)
-B2 = matrix_inverse_2(B, det_B)
-
-def matrix_inverse_3(mataorb, detaorb):
-    return 1/detaorb * mataorb[0]
-
-A3 = matrix_inverse_3(A, det_A)
-B3 = matrix_inverse_3(B, det_B)
-
-Ainv = [A0, A1, A2, A3]
-Binv = [B0, B1, B2, B3]
-
-print ("Matrix A inverted is:", Ainv)
-print ("Matrix B inverted is:", Binv)
 
 #matrix summ
 
@@ -86,27 +49,52 @@ multmatrix = matrix_multmatrix(A, B)
 
 print('Mult of matrix A and B is', multmatrix)
 
-#matrix inverse test
-
-invtestA = matrix_multmatrix(A, Ainv)
-invtestB = matrix_multmatrix(B, Binv)
-
-print (invtestA, invtestB)
-
-def matrixinvcheck(matrix, invmatrix):
-    if matrix_multmatrix(matrix, invmatrix) == [1, 0, 0, 1]:
-        print ('OK')
-    else:
-        print('Not OK')
-
-matrixinvcheck(A, Ainv)
-matrixinvcheck(B, Binv)
 
 #matrix trans
 
 Btrans = [B[0], B[2], B[1], B[3]]
 
 print ('B trns is', Btrans)
+
+#matrix determ func 2
+
+def matrix_determ(mataorb):
+    return mataorb[0] * mataorb[3] - mataorb[2] * mataorb[1]
+
+det_A = matrix_determ(A)
+det_B = matrix_determ(B)
+
+print ("Determent of matrix A is:",det_A)
+print ("Determent of matrix B is:",det_B)
+
+#Matrix inverse pos check det != 0
+
+def matrix_inverse(mataorb, detaorb):
+    if detaorb == 0:
+        raise SystemExit("Matrix", mataorb, "cannot be inverted, determent = 0.")
+    else:
+        return [(1 / detaorb * mataorb[3]), (1 / detaorb * -1 * mataorb[1]), (1 / detaorb * -1 * mataorb[2]), (1 / detaorb * mataorb[0])]
+
+
+Ainv = matrix_inverse(A, det_A)
+Binv = matrix_inverse(B, det_B)
+
+print("Matrix A inverted is:", Ainv)
+print("Matrix B inverted is:", Binv)
+
+#matrix inverse test
+
+invtestA = matrix_multmatrix(A, Ainv)
+invtestB = matrix_multmatrix(B, Binv)
+
+def matrixinvcheck(matrix, invmatrix):
+    if matrix_multmatrix(matrix, invmatrix) == [1, 0, 0, 1]:
+        print ('Matrix',matrix,'invert check OK')
+    else:
+        print('Matrix' ,matrix,'invert check NOT OK')
+
+matrixinvcheck(A, Ainv)
+matrixinvcheck(B, Binv)
 
 result = [matrix_sum(matrix_sum(matrix_sum(matrix_multint(Ainv, det_B),  matrix_multmatrix(A, Btrans)), matrix_multint(matrix_multmatrix(B, B), -3)), matrix_multint(Binv, det_A))]
 
@@ -115,3 +103,31 @@ print('Home task result is', result)
 print('-------------------END OF 2x2 MATRICES-------------------')
 
 #matrix determ func 3
+
+#3x3 block
+
+#input block matrix
+
+#inp = input('Add Matix A, separated by space:')
+#A = list(map(float, inp.split()))
+
+A = [1.0, -5.0, 12.0, 7.0, 6.0, 2.0, 3.0, 9.0, 4.0]
+
+print ('Matrix A is:')
+print ('(', A[0], A[1], A[2],')')
+print ('(', A[3], A[4], A[5],')')
+print ('(', A[6], A[7], A[8],')')
+
+#Det 3x3 first way
+
+det3x3_1 = ((A[0] * A[4] * A[8]) + (A[1] * A[5] * A[6]) + (A[3] * A[7] * A[2])) - ((A[6] * A[4] * A[2]) + (A[7] * A[5] * A[0]) + (A[3] * A[1] * A[8]))
+
+print (det3x3_1)
+
+def matrix3_multinv(matrix, inte):
+    return [matrix[0] * inte , matrix[3] * inte, matrix[6] * inte, matrix[1] * inte, matrix[4] * inte, matrix[7] * inte, matrix[2] * inte, matrix[5] * inte, matrix[8] * inte]
+
+inte = 1 / det3x3_1
+multinv = matrix3_multinv(A, inte)
+
+print(inte, 'Matrix', A , 'invert is', multinv)
